cda01d17f412e71914be1c1eb2343d15a7b636d0
imkarthi16 <karthiveera@gmail.com>
2022-09-06 18:12:31 +0530

    working with react three fiber

---
+++ src/components/header.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header>
+      <div className='logo'>
+        <span>REACT THREE FIBER</span>
+      </div>
+      <div className='episode'>
+        <span>Karthick</span>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
